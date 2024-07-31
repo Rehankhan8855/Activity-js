04560683796b33a122e09faa964c80bfd3e4e7d9
Rehankhan8855 <rehankhan27587@gmail.com>
2024-07-31 12:00:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Activity-js
+ mathematical operations
---
+++ main.js
@@ -0,0 +1,48 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const result = document.getElementById("result");
+
+        function add(){
+            result.innerHTML = parseInt(num1.value) + parseInt(num2.value);
+
+        }
+
+
+const num3=document.getElementById("num3")
+const num4=document.getElementById("num4")
+const result2=document.getElementById("result2")  
+
+   function subtract() {
+    result2.innerHTML = parseInt(num3.value) - parseInt(num4.value)
+
+   }
+
+const num5 = document.getElementById("num5")
+const num6 = document.getElementById("num6")
+const result3 = document.getElementById("result3")
+
+    div = () =>{
+        result3.innerText = parseInt(num5.value) / parseInt(num6.value)
+    }
+
+
+const num7 = document.getElementById("num7")
+const num8 = document.getElementById("num8")
+const result4 = document.getElementById("result4")
+
+  multi = () =>{
+    result4.innerText = parseInt(num7.value) * parseInt(num8.value)
+    
+  }
+
+ 
+const num9 = document.getElementById("num9")
+const num10 = document.getElementById("num10")
+const result5 = document.getElementById("result5")
+
+   modul = () => {
+    result5.innerText = parseInt(num9.value) % parseInt(num10.value)
+
+   }
+
+
